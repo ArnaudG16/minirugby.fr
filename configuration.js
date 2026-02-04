31ab0d2dbfa7f4c432d0da89a893d73d9e4dc036
Arnaud Grassian <arnaud.vy16@gmail.com>
2026-02-04 02:23:04 +0100

    Config

---
+++ configuration.js
@@ -0,0 +1,13 @@
+
+
+const projectInfo = {
+  name: "Mini Rugby",
+  url: "https://minirugby.fr",
+  description: "Dépôt d'images et de ressources",
+};
+
+function displayStatus() {
+  console.log(`Projet ${projectInfo.name}`);
+}
+
+displayStatus();
